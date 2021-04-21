1e085507cbbaf4fd9dab913fcd61a5940e423dcb
Lucas Gabriel <lucas.ddd@hotmail.com>
2021-04-20 23:45:50 -0300

    Plan MVC - Create

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            #filtros
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preco</th>
+                        <th style="width: 50px">Acoes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>
+                                {{ $plan->name }}
+                            </td>
+                            <td>
+                                {{ $plan->price }}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-warning">VER</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {!! $plans->links() !!}
+        </div>
+    </div>
+@stop
